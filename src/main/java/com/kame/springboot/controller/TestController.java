e3985d1cd8246d2e988c57f7b1b7c47da404d3e5
S Kame <skame@kameyamamisanoiMac.local>
2021-11-06 18:17:46 +0900

    7-1 OK validation

--- src/main/java/com/kame/springboot/controller/TestController.java
+++ src/main/java/com/kame/springboot/controller/TestController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,41 +16,30 @@ import com.kame.springboot.service.UserDetailsServiceImpl;
 @Controller
 @RequestMapping("/")
 public class TestController {
-	
-	@Autowired
+    @Autowired
     private UserDetailsServiceImpl userDetailsServiceImpl;
-	
-	@GetMapping
-    public String index () {
+
+    @GetMapping
+    public String index() {
         return "index";
     }
 
     @GetMapping("/login")
-    public String login () {
+    public String login() {
         return "login";
     }
-    
-    /**
-     * 「ユーザー登録」ページを表示するだけ
-     * @param signupForm
-     * @return "signup"
-     */
+
     @GetMapping("/signup")
     public String newSignup(SignupForm signupForm) {
         return "signup";
     }
 
-    /**
-     * ユーザー情報の登録実行
-     * UserDetailsServiceImpl において作成した register メソッドを使用して
-     * ユーザー情報をデータベースに格納します
-     * ユーザー登録に失敗した場合は、「ユーザー登録に失敗しました」というメッセージを表示する 既に登録されているユーザー名で登録しようとすると、とりあえずエラーメッセージが表示
-     * @param signupForm
-     * @param model
-     * @return 成功:"redirect:/"<br /> 失敗:"signup"
-     */
     @PostMapping("/signup")
-    public String signup(SignupForm signupForm, Model model) {
+    public String signup(@Validated SignupForm signupForm, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "signup";
+        }
+
         try {
             userDetailsServiceImpl.register(signupForm.getUsername(), signupForm.getPassword(), "ROLE_USER");
         } catch (DataAccessException e) {
--- src/main/java/com/kame/springboot/form/SignupForm.java
+++ src/main/java/com/kame/springboot/form/SignupForm.java
@@ -1,7 +1,25 @@
 package com.kame.springboot.form;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
+// フォームクラスには エンティティのアノテーションはつけない
 public class SignupForm {
+	
+	
+	// フォームには、入力チェックを行うためのアノテーションを付けていきます
+	//Null チェックには、一般的には ＠NotNull を使いますが、ここでは、＠NotBlank を使用しています。
+//	＠NotNull 又は ＠NotEmpty を使用した場合、半角スペースのみでもユーザー名として登録ができてしまいますが、
+//	この半角スペースのユーザー名ではログインすることができないためです。
+	// なお、Spring MVC では「文字列の入力フィールドに未入力の状態でフォームを送信した場合、デフォルトではフォームオブジェクトにnullではなく、空文字がバインドされる」ため、＠NotNull は入力値のチェックでは働きません。
+	
+	 @NotBlank // これにしてください
+	    @Size(min = 1, max = 50, message = "ユーザー名は1文字以上50文字以下で入力してください")  // 入力文字数の最小値と最大値を指定
     private String username;
+	 
+	 @NotBlank  //  Null、空文字、空白をエラーとする これにしてください
+	    @Size(min = 6, max = 20)
     private String password;
 
     public String getUsername() {
